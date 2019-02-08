906f690d378ae896b4f1fc0a79de94d58b9d9473
bigjoze <joseph_mce@live.co.uk>
2019-02-08 20:33:50 +0000

    tidied up formatting, removed unecessary code, fixed the loading section not displaying correctly by adding a couple divs: <div className=module1></div><div className=mainmodule>

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,57 +1,46 @@
 import React from "react";
 import { browserHistory } from "react-router";
-import {Link} from "react-router";
+import { Link } from "react-router";
 
 const API = 'https://itunes.apple.com/search?term=rock&media=music';
 
 export class Home extends React.Component {
-    
-    //ES6 - Check out
     constructor(props) {
         super();
-        //React Property that sets our state
         this.state = {
             //You shouldn't assign props to your state, except for the case where this is the initial value
             results: [],
             isLoading: false
         };
-
     }
-
     componentDidMount() {
         this.setState({ isLoading: true });
-
         fetch(API)
-        .then(response => response.json())
-        .then(data => this.setState({ results: data.results, isLoading: false }));
+            .then(response => response.json())
+            .then(data => this.setState({ results: data.results, isLoading: false }));
         console.log("Component did mount!");
     }
-    onNavigateDetails(){
+    onNavigateDetails() {
         browserHistory.push("/Details");
-    } 
-
+    }
     render() {
         const { results, isLoading } = this.state;
-
         if (isLoading) {
             return <div className="module1">
-                <div className="config">
-                    <div className="gridContainer">
-                        <div className="grid-1" >
-                        <img src={require('../Images/loading.gif')}/>                      
-                            </div>
-                        </div>
-                    </div>
-                </div>;
-          }
-        return (
-            <div className="module1">
-                <div>                   
-                </div>
-
                 <div className="mainmodule">
                     <div className="gridContainer">
-                        <div className="grid-1" >                        
+                        <div className="grid-1" >
+                            <img src={require('../Images/loading.gif')} />
+                        </div>
+                    </div>
+                </div>
+            </div>;
+        }
+        return (
+            <div className="module1">
+                <div className="mainmodule">
+                    <div className="gridContainer">
+                        <div className="grid-1" >
                         <ul >
                             {results.map(track =>
                                 <li className ="list" key={track.collectionId}>    
@@ -76,36 +65,12 @@ export class Home extends React.Component {
                                     <div className="linebreak"></div>
                                 </li>
                             )}
-                        </ul>                        
-                            </div>
+                        </ul>
                         </div>
-
                     </div>
                 </div>
-               
-           
+            </div>
         );
     }
 }
-Home.propTypes = {
-
-};
-
-
-/*
-<br/>
-                    <hr/>
-                        <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
-                        <hr/>
-                        <input type="text" value={this.state.homeLink} onChange={(event) => this.onHandleChange(event)} />                       
-                        <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
-                        <hr/>
-                        */
-
-
-
-//Button on click could also be this ~~~~~ <button onClick={this.onMakeOlder.bind(this)} className ="btn btn-primary">Make me older!</button>
-
-//{this.props.name}, Age: {this.state.age} <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
-
-//Status: {this.state.status}
+//JM - 08-02-2019 tidied up formatting, removed unecessary code, fixed the loading section not displaying correctly by adding a couple divs: <div className="module1"></div><div className="mainmodule">
