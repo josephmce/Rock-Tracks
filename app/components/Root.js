aaaf2c5b33a31bc89087bdecac540016461b106f
bigjoze <joseph_mce@live.co.uk>
2019-02-09 09:05:08 +0000

    Removed unused code

--- app/components/Root.js
+++ app/components/Root.js
@@ -13,18 +13,6 @@ export class Root extends React.Component{
             homeMounted: true
         };
     }
-    
-    onGreet()
-    {
-        alert("Hello");
-    }
-
-    onChangeLinkName(newName){
-        this.setState({
-            homeLink: newName
-        });
-    }
-
     onChangeHomeMounted(){
         this.setState({
             homeMounted: !this.state.homeMounted
@@ -38,7 +26,6 @@ export class Root extends React.Component{
             );
         }
         return(
-
             <div>
                 <Sidebar/>        
                     <div> 
@@ -50,15 +37,4 @@ export class Root extends React.Component{
             </div>
         );
     }
-
 }
-
-/*
-                    <div className="container">
-                        <div className="row">         
-                            <div className="col-xs-10 col-xs-offset-1">
-                                <button onClick={this.onChangeHomeMounted.bind(this)} className ="btn btn-primary" >(Un)Mount Home Component</button>                       
-                            </div>
-                        </div>
-                    </div>
-                    */
